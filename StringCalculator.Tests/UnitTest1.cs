befe9e9526e282172e048bba32bf07177c5041b1
Siphiwe <siphiwe.manda@bepsystems.co.uk>
2021-03-02 14:42:47 +0000

    Step one and step two
    String calc

--- StringCalculator.Tests/UnitTest1.cs
+++ StringCalculator.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using stringCalculator;
 using Xunit;
 
 namespace StringCalculator.Tests
@@ -6,8 +7,44 @@ namespace StringCalculator.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GivenAnEmptyStringReturnZero()
         {
+            //Arrange
+            var stringCalculator = new StringCalculatorClass();
+            //Act 
+            var emptyString = stringCalculator.Calculate("");
+            //Assert
+            Assert.Equal(0, emptyString);
+        }
+        [Fact]
+        public void GivenASingleNumberReturnNumber()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculatorClass();
+            //Act 
+            var oneNumber = stringCalculator.Calculate("4");
+            //Assert
+            Assert.Equal(4, oneNumber);
+        }
+        [Fact]
+        public void TwoNumbersReturnThereAddition()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculatorClass();
+            //Act 
+            var oneNumber = stringCalculator.Calculate("4,4");
+            //Assert
+            Assert.Equal(8, oneNumber);
+        }
+        [Fact]
+        public void AnyNumberOfNumbersReturnsThereAddition()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculatorClass();
+            //Act 
+            var manyNumbers = stringCalculator.Calculate("1,2,3,4,5,6,7,8,9");
+            //Assert
+            Assert.Equal(45, manyNumbers);
         }
     }
 }
---
+++ stringCalculator/StringCalculatorClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace stringCalculator
+{
+    public class StringCalculatorClass
+    {
+        public int Calculate(string parameters)
+        {
+            return string.IsNullOrEmpty(parameters) ? 0 : Addition(parameters);
+        }
+
+        private int Addition(string parameters)
+        {
+
+            var array = parameters.Replace(",", String.Empty).Select(x => int.Parse(x.ToString())).ToArray();
+            return array.Sum();
+        }
+    }
+}
